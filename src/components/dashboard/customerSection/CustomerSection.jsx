4b015ac859b4adf38f2289d14bde61835804c153
Daniel Hermansen <daniel.j.hermansen@gmail.com>
2021-01-28 20:19:17 -0700

    Restructured the component folders within dashboard and implemented styled-components in the folders

---
+++ src/components/dashboard/customerSection/CustomerSection.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import SmallItemCard from '../../common/SmallItemCard';
+import { WechatOutlined, PlusOutlined } from '@ant-design/icons';
+import styled from 'styled-components';
+
+export function CustomerSection() {
+  return (
+    <StyledCustomerSection>
+      <div className="customerHeader">
+        <h2>Customers</h2>
+        <div>
+          <WechatOutlined />
+          <PlusOutlined />
+        </div>
+      </div>
+
+      <p>Information</p>
+      <SmallItemCard headerText={36} descText="Customers" />
+    </StyledCustomerSection>
+  );
+}
+
+const StyledCustomerSection = styled.div`
+  .customerHeader {
+    display: flex;
+    justify-content: space-between;
+  }
+`;
