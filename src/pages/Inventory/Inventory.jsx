035101d5e8df6e916fbe46488bd8b0595d29c841
ryanp <ryanpanimation@gmail.com>
2021-02-11 02:06:37 -0600

    create functions to pass to DataTable for actions

--- src/pages/Inventory/Inventory.jsx
+++ src/pages/Inventory/Inventory.jsx
@@ -1,47 +1,53 @@
 import React, { useEffect, useState } from 'react';
-// import { Button } from 'antd';
 import { Button } from '../../components/common/Button';
 import { Link } from 'react-router-dom';
 
-// import { SearchResults } from '../../components/inventory/SearchResults';
-// import useSearch from '../../hooks/useSearch';
 import { useFetch } from '../../hooks/useFetch';
-import { useOktaId } from '../../hooks/useOktaId';
 import { Layout } from '../../components/common/Layout/Layout';
 import { DataTable } from '../../components/common/DataTable';
 
-export function CurrentInventory() {
-  // const [searchData] = useState({});
+import { useProfile } from '../../contexts/profile/ProfileProvider';
+
+export function Inventory() {
+  const { stores } = useProfile();
   const [inventory, setInventory] = useState([]);
 
-  const { oktaId } = useOktaId();
-  const { get } = useFetch();
+  const { get, deleteReq } = useFetch();
 
   useEffect(
     function fetchInventory() {
       async function asyncFetch() {
-        const allStores = await get(`profiles/${oktaId}/stores`);
-        const firstStoreID = await allStores.data[0].id;
-        const firstStoreData = await get(`stores/${firstStoreID}/products`);
+        const firstStoreData = await get(`stores/${stores[0].id}/products`);
         const firstStoreInventory = await firstStoreData.data;
         setInventory([...firstStoreInventory]);
-        console.log(`set inventory, ${firstStoreInventory.length} items`);
       }
 
-      if (oktaId) {
+      if (stores.length > 0) {
         asyncFetch();
       }
     },
-    [oktaId, get]
+    [stores, get]
   );
 
-  // const displayedData = useSearch(inventory, 'name', searchData);
+  const handleEdit = id => {
+    // go to edit product page and use this products info
+    console.log(id);
+  };
+  const handleList = id => {
+    console.log(id);
+  };
+  const handleDelete = async id => {
+    console.log(id);
+    await deleteReq(`products/${id}`);
+    const firstStoreData = await get(`stores/${stores[0].id}/products`);
+    const newFirstStoreInventory = await firstStoreData.data;
+    setInventory([...newFirstStoreInventory]);
+  };
 
   return (
     <Layout>
       <div className="outerContainer">
         <div className="contents">
-          {/* <SearchResults data={inventory} filter={searchData} /> */}
           <Link to="/myprofile/inventory/additem">
             <Button>+Add Item</Button>
           </Link>
@@ -51,6 +57,7 @@ export function CurrentInventory() {
             columns={['Name', 'Created_At', 'Price', 'Stock_Quantity']}
             inputData={inventory}
             actions={['Edit', 'List Item', 'Delete']}
+            funcs={[handleEdit, handleList, handleDelete]}
           />
         </div>
       </div>
